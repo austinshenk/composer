d5fd5686d6035d3a3953bef2703f97a01ee70cf1
ashenk <ashenkster69@gmail.com>
2016-02-22 22:13:51 -0500

    Add unstaged files

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var compression = require('compression');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(compression());
+app.use(express.static(__dirname + '/assets'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/reader', function(request, response) {
+  response.render('pages/reader');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
