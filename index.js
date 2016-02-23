9f9c8a71944fa87a752b233b7de328c3cf29a507
ashenk <ashenkster69@gmail.com>
2016-02-22 22:01:07 -0500

    Remove compression module and use express built in one

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var express = require('express');
-var compression = require('compression');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.compress());
 app.use(express.static(__dirname + '/assets'));
-app.use(compression());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
