9001727f27c94832b130da2dacd00c75de31faa4
ashenk <ashenkster69@gmail.com>
2016-02-22 22:05:06 -0500

    Add compression back in because express is not bundled with it anymore

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var compression = require('compression');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.compress());
+app.use(compression());
 app.use(express.static(__dirname + '/assets'));
 
 // views is directory for all template files
